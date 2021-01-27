e91e8d9ad8ab7aa028b364f3c6df4da4537abea0
Lelio <lelio.rollin@e-artsup.net>
2021-01-27 09:59:03 +0100

    First proto

---
+++ Assets/Scripts/Collec.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collec : MonoBehaviour
+{
+   public ScoreManager scoreManager;
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (other.tag == "Player")
+      {
+         scoreManager.score++;
+         
+         Destroy(gameObject);
+      }
+   }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public List<GameObject> collec = new List<GameObject>();
+
+    public float score = 0;
+
+    public TextMeshProUGUI text;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = score + "/ 5";
+
+        if (score == 5 || Input.GetKeyDown(KeyCode.R))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
